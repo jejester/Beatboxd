3773bd406abbca5bf66977242a6e68d603de5e3f
Jester <jesterdc41@gmail.com>
2024-09-26 15:51:29 +0800

    Installed Vue and MongoDB

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 import './bootstrap';
+import { createApp } from 'vue/dist/vue.esm-bundler.js';
+
+// Import your Vue component
+import ExampleComponent from './components/ExampleComponent.vue';
+
+// Create Vue app and register the component
+const app = createApp({});
+app.component('example-component', ExampleComponent);
+
+// Mount the Vue app to the DOM element with ID 'app'
+app.mount('#app');
---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Hello from Vue.js with Vite in Laravel!</h1>
+        <h1>Test</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ExampleComponent',
+};
+</script>
+
+<style scoped>
+h1 {
+    color: #42b983;
+}
+</style>
